5d5bdc8d4f4cd5cceac8ba7a8022cdca83aca5a0
JunKyle <claudine.manrique@gmail.com>
2022-12-16 14:07:29 +0100

    First commit : env config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faVideoCamera } from '@fortawesome/free-solid-svg-icons'
+library.add(faLocationDot, faVideoCamera)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
